bab696362f6fac66141e119eeff6f12e5d8da321
Светлана <se.berseneva@gmail.com>
2022-12-12 16:50:49 +0300

    Сделала слайдер рабочий с input

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
+// var slider = document.getElementById('slider');
+//     noUiSlider.create(slider, {
+//         start: [20, 80],
+//         connect: true,
+//         range: {
+//             'min': 0,
+//             'max': 100
+//                 }
+// });
+       
 
-                            var slider = document.getElementById('slider');
-                                noUiSlider.create(slider, {
-                                    start: [20, 80],
-                                    connect: true,
-                                    range: {
-                                        'min': 0,
-                                        'max': 100
-                                    }
-                                });
-                        
+    const rangeSlider = document.getElementById("range-slider");
+
+    if (rangeSlider) {
+        noUiSlider.create(rangeSlider, {
+            start: [5, 8],
+            connect: true,
+            step: 1,
+            range: {
+                'min': 1,
+                'max': 10
+                    }
+    });
+
+    const input0 = document.getElementById("input-0");
+    const input1 = document.getElementById("input-1");
+    const inputs = [input0, input1];
+
+    rangeSlider.noUiSlider.on("update", function(values, handle) {
+        inputs[handle].value = Math.round(values[handle]);
+    });
+
+    const setRangeSlider = (i, value) => {
+        let arr = [null, null];
+        arr[i] = value;
+        console.log(arr);
+        rangeSlider.noUiSlider.set(arr)
+    };
+
+    inputs.forEach((el, index) => {
+        el.addEventListener("change", (e)=> {
+            console.log(index)
+        setRangeSlider(index, e.currentTarget.value);
+        });
+        
+    })
+    
+    }
